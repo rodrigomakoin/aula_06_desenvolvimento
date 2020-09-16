7ac589029c96e0a2a8540a8116f4557979edcc9c
rodrigomakoin <48112596+rodrigomakoin@users.noreply.github.com>
2020-09-16 19:34:41 -0300

    Add files via upload

---
+++ atv.php
@@ -0,0 +1,14 @@
+<?php
+
+$num1 =$_GET['num1'];
+$num2 =$_GET['num2'];
+
+$soma = $num1 + $num2
+$sub = $num1 - $num2
+$mult = $num1 * $num2
+$div = $num1 / $num2
+
+echo "Soma: $soma <br>";
+echo "Subitração: $sub <br>";
+echo "Multiplicação: $mult <br>";
+echo "Divisão: $div <br">;
